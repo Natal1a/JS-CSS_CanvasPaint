4bdd469a28f295ce9ac92d832613ddc94bb43e9d
Natalia <natalia132@wp.pl>
2018-05-09 23:20:01 +0200

    color and size set

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+const inputs = document.querySelectorAll('.controls input');
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
@@ -8,25 +9,32 @@ ctx.lineCap = 'round';
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
-ctx.strokeStyle = '#BADA55';
-ctx.lineWidth = 100;
 
 
 canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
   [lastX, lastY] = [e.offsetX, e.offsetY]
 });
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', () => isDrawing = false);
-canvas.addEventListener('mouseout', () => isDrawing = false);
-
 
 function draw(e){
   if (!isDrawing) return;
-  console.log(e);
+  // console.log(e);
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY]
 }
+
+function handleChange(){
+  ctx.strokeStyle = `${this.value}`;
+  ctx.lineWidth = `${this.value}`;
+  // console.log(this.value);
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+
+inputs.forEach(input => input.addEventListener('change', handleChange));
+inputs.forEach(input => input.addEventListener('mausemove', handleChange));
